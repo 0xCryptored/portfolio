d9f9854fb873debc53ce622398a8e36907fa49ed
0xcryptored <nalanarud@gmail.com>
2023-04-25 00:50:07 -0500

    Copy functionality added, footer added, additional CSS added

--- encryptor/myscript.js
+++ encryptor/myscript.js
@@ -1,7 +1,6 @@
 const input = document.getElementById('input-text');
 const output = document.getElementById('output-text');
 
-
   // Encrypt Function
   function encrypt() {
 
@@ -19,34 +18,76 @@ const output = document.getElementById('output-text');
 
       // Actualizar el valor del textarea "ouput" al texto transformado
       output.value = textoEncriptado;
+      alert("Encriptado!", 3000)
 
     }
 
   // Decrypt Function
   function decrypt() {
-        // Get elemento de la textarea Input
-        const input = document.getElementById('input-text');
-
-        // Get elemento de la textarea Output
-        const output = document.getElementById('output-text');
-
-        // Get valor inicial del texto del Input
-        textoInicial = input.value;
-
-        //Transformar el texto original, usando funcion replaceAll()
-        let textoDesencriptado = textoInicial.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
-
-        // Actualizar el valor del textarea "ouput" al texto transformado
-        output.value = textoDesencriptado;
+    // Get elemento de la textarea Input
+    const input = document.getElementById('input-text');
+    
+    // Get elemento de la textarea Output
+    const output = document.getElementById('output-text');
+    
+    // Get valor inicial del texto del Input
+    textoInicial = input.value;
+    
+    //Transformar el texto original, usando funcion replaceAll()
+    let textoDesencriptado = textoInicial.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+    
+    // Actualizar el valor del textarea "ouput" al texto transformado
+    output.value = textoDesencriptado;
+    alert("Desencriptado!", 3000)
   }
 
   // BOTON DE COPIADO ============
+  function copy() {
+    // Get el campo de texto
+    var copyText = document.getElementById("output-text");
+  
+    // Seleccionar el campo
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
 
-  // ANIMACION SECRETA CAPIBARA ============
-  function secret(){
-    title = "Capibara!";
-    document.getElementById('capi').innerHTML = title;
+     // copiar el texto dentro del campo
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Se copio el texto: " + copyText.value, 3000);
   }
+
+  // CAMBIOS SECRETOS CAPIBARA ============
+  function secret(){
+    let capi = "Capibara!";
+    const title = document.getElementById('capi');
+    if (title.innerHTML === "Cryptored Labs"){
+      title.innerHTML = capi;
+    }else{
+      title.innerHTML = "Cryptored Labs";
+    }
+  }
+
+  // SHOW ALERT
+  function alert(message, delay) {
+    var alert = document.getElementById("alert");
+    alert.innerHTML = message;
+    alert.style.display = "inline-block";
+    setTimeout(function(){
+      alert.style.display = "none";
+    }, delay);
+  }
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const copyButton = document.querySelector('.copyButton');
+    copyButton.onclick = () => {
+      copy();
+      secret();
+    }
+  });
+  
+
   
   
 
